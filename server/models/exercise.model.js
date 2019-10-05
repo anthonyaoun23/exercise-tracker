9b958c1854f3704fa3f18625e0489702e2856339
Anthony <anthony.aoun1@hotmail.com>
2019-10-05 11:54:19 -0400

    add routes

--- server/models/exercise.model.js
+++ server/models/exercise.model.js
@@ -14,6 +14,6 @@ const exerciseSchema = new Schema(
   }
 );
 
-const Exercise = mongoose.model("User", userSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 module.exports = Exercise;
